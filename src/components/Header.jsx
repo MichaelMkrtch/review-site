11361e4c3badae425adb7c17bdd49e1338c5676b
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2023-11-29 16:12:37 -0800

    build: add Prettier plugin for Tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 export default function Header() {
   return (
     <header>
-      <div className="flex justify-between items-center px-12 py-8">
+      <div className="flex items-center justify-between px-12 py-8">
         <h1 className="text-4xl">Reviews</h1>
-        <button className="text-base bg-cyan-350 px-4 py-2 rounded">Add Review</button>
+        <button className="rounded bg-cyan-350 px-4 py-2 text-base">
+          Add Review
+        </button>
       </div>
     </header>
   );
