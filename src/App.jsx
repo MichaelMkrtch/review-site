975920d43ec4bfc61944f1e0b8911cb598e5435e
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2023-12-28 17:19:37 -0800

    feat: start add review functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,46 @@ import { useState } from "react";
 import Reviews from "./components/Reviews.jsx";
 import Modal from "./components/Modal.jsx";
 import Search from "./components/search/Search.jsx";
+import AddReview from "./components/AddReview.jsx";
 
 function App() {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [searchModalIsOpen, setSearchModalIsOpen] = useState(false);
+  const [reviewModalIsOpen, setReviewModalIsOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
 
+  function handleStartSearch() {
+    setSearchModalIsOpen(true);
+  }
+
+  function handleStopSearch() {
+    setSearchModalIsOpen(false);
+  }
+
   function handleStartAddReview() {
-    setModalIsOpen(true);
+    setReviewModalIsOpen(true);
   }
 
   function handleStopAddReview() {
-    setModalIsOpen(false);
+    setReviewModalIsOpen(false);
   }
 
   return (
     <>
       <Modal
-        open={modalIsOpen}
-        onClose={handleStopAddReview}
+        open={searchModalIsOpen}
+        onClose={handleStopSearch}
         setSearchTerm={setSearchTerm}
       >
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </Modal>
-      <Reviews onAddReview={handleStartAddReview} />
+      <Modal
+        open={reviewModalIsOpen}
+        onClose={handleStopAddReview}
+        setSearchTerm={setSearchTerm}
+      >
+        <AddReview />
+      </Modal>
+      <Reviews onSearch={handleStartSearch} />
     </>
   );
 }
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,13 +1,13 @@
-export default function Reviews({ onAddReview }) {
+export default function Reviews({ onSearch }) {
   return (
     <>
       <div className="flex items-center justify-between px-12 py-8">
         <h1 className="text-4xl">Reviews</h1>
         <button
           className="rounded bg-cyan-350 px-4 py-2 text-base outline-none transition-colors duration-200 ease-in-out hover:bg-cyan-250"
-          onClick={onAddReview}
+          onClick={onSearch}
         >
-          Add Review
+          Search
         </button>
       </div>
     </>
