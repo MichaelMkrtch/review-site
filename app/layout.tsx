591b363b7764ea5d1f9d4fb6479c9aa2d5946f80
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2024-01-26 21:21:43 -0800

    build: add Tailwind plugin to Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,23 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { Sora, Plus_Jakarta_Sans } from 'next/font/google'
-import NavMenu from '@/components/NavMenu';
+import type { Metadata } from "next";
+import "./globals.css";
+import { Sora, Plus_Jakarta_Sans } from "next/font/google";
+import NavMenu from "@/components/NavMenu";
 
 const sora = Sora({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-sora',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-sora",
+});
 
 const plusJakartaSans = Plus_Jakarta_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-jakarta',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-jakarta",
+});
 
 export const metadata: Metadata = {
-  title: 'Review Site',
-  description: 'A place to share your movie reviews',
+  title: "Review Site",
+  description: "A place to share your movie reviews",
 };
 
 export default function RootLayout({
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body>
         <NavMenu />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
