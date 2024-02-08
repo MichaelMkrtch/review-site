158baaccfa5e7586ddb27d361198157037f18f41
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2024-02-07 22:32:33 -0800

    fix: update library page to access local storage

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,30 +1,38 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import { IMG_BASE_URL } from "@/secrets";
 import Image from "next/image";
 
 export default function Library() {
-  const allKeysJSON = Object.keys(localStorage);
-  const reviewsJSON = allKeysJSON.map((key) => localStorage.getItem(key));
-  const reviews = reviewsJSON.map((review) => {
-    if (review) return JSON.parse(review);
-  });
+  const [reviews, setReviews] = useState<any[]>();
+
+  useEffect(() => {
+    const allKeysJSON = Object.keys(localStorage);
+    const reviewsJSON = allKeysJSON.map((key) => localStorage.getItem(key));
+    const parsedReviews = reviewsJSON.map((review) => {
+      if (review) return JSON.parse(review);
+    });
+    setReviews(parsedReviews);
+  }, [reviews]);
 
   return (
-    <div className="flex">
-      {reviews.map((review) => {
-        return (
-          <Image
-            key={review.id}
-            src={`${IMG_BASE_URL}w500${review.posterPath}`}
-            alt="Movie poster"
-            width={160}
-            height={240}
-            className="h-60 w-40 mx-4 rounded"
-            priority
-          />
-        );
-      })}
+    <div className="flex flex-wrap">
+      {reviews &&
+        reviews.map((review) => {
+          return (
+            <Image
+              key={review.id}
+              src={`${IMG_BASE_URL}w342${review.posterPath}`}
+              alt="Movie poster"
+              width={160}
+              height={240}
+              className="mx-4 h-60 w-40 rounded"
+              priority
+            />
+          );
+        })}
     </div>
   );
 }
